c53d482e56f943a403e42e7e4a0baade0a5b1aab
Vinicius Castro da Costa <48870801+vinicastroo@users.noreply.github.com>
2022-01-16 00:04:17 -0200

    [WEB] Configuração Error
    Melhorar visualização dos erros caso aconteça

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
 export function App(){
+    throw new Error('Eita porra');
+
     return <h1>Hello O</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
     mode: 'development',
+    devtool: 'eval-source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
