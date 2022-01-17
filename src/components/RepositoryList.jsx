f2116aa344bc5581b9605228a6e27e42b466363d
Vinicius Castro da Costa <48870801+vinicastroo@users.noreply.github.com>
2022-01-17 11:06:19 -0200

    [WEB] Configurando Fast Refresh

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,22 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'forms in react',
+    link: 'https://github.com/vinicastroo',
+}
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+            </ul>
+        </section>
+    )
+}
